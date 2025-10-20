9d6027891f1a23c673c1ab937d6ac9851d60f9f7
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-10-19 20:19:29 -0600

    Actualiza la clase CuentasCredito

--- AplicacionCuentasBancarias/src/CuentasCredito.java
+++ AplicacionCuentasBancarias/src/CuentasCredito.java
@@ -88,4 +88,41 @@ public class CuentasCredito {
     public boolean equals(CuentasCredito cuenta) {
         return (id.equals(cuenta.id));
     }
+
+    //Funcionales
+    public void retiro(double monto) {
+        if (!estaActiva()) {
+            System.out.println("Operación no válida.");
+            System.out.println("La cuenta de crédito " + id + " está inactiva.");
+            return;
+        }
+        if (saldo + -1 * monto > 0.0) {
+            System.out.println("Operación no válida.");
+            System.out.println("Las cuentas de crédito no pueden tener un saldo mayor a $0.");
+            System.out.println("El retiro solicitado no se realizó.");
+            return;
+        }
+        if (saldo + -1 * monto < -1 * limiteCredito) {
+            System.out.println("Operación no válida.");
+            System.out.println("Las cuentas de crédito no pueden tener un saldo mayor al límite de crédito.");
+            System.out.println("El retiro solicitado no se realizó.");
+            return;
+        }
+        setSaldo(saldo + -1 * monto);
+    }
+
+    public void pago(double monto) {
+        if (!estaActiva()) {
+            System.out.println("Operación no válida.");
+            System.out.println("La cuenta de crédito " + id + " está inactiva.");
+            return;
+        }
+        if (saldo + -1 * monto < -1 * limiteCredito) {
+            System.out.println("Operación no válida.");
+            System.out.println("Las cuentas de crédito no pueden ejecutar un pago que exceda el límite de crédito.");
+            System.out.println("El pago solicitado no se realizó.");
+            return;
+        }
+        setSaldo(saldo + -1 * monto);
+    }
 }
